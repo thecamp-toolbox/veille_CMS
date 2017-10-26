f6a8ba95d3d8eb611a23eec9a24a3b12ed975194
Arthur Schmitt <tart2000design@gmail.com>
2017-10-26 17:13:55 +0200

    now with navigation between collections

--- site/templates/collection.php
+++ site/templates/collection.php
@@ -1,14 +1,31 @@
 <?php snippet('header') ?>
 
-    <section class="jumbotron text-center">
-	    <div class="container">
-	    	<h1 class="jumbotron-heading"><?= $page->title() ?></h1>
-	    	<p class="lead text-muted"><?= $page->text()->kirbytext() ?></p>
-	    	<?php if ($site->user()) : ?>
-	    		<p class="text-muted"><a href="<?= $site->url().'/panel/pages/'.$page->uri().'/edit' ?>" target="_blank">Edit</a></p>
-	    	<?php endif ?>
+
+	    <div class="container-fluid jumbotron">
+	    	<div class="row">
+		    	<div class="col-sm-2 text-center">
+		    		<?php if($page->hasPrevVisible()): ?>
+						<a href="<?php echo $page->prevVisible()->url() ?>">
+							<i class="arrow fa fa-caret-left"></i>
+						</a>
+					<?php endif ?>
+		    	</div>
+		    	<div class="col-sm-8 text-center">
+			    	<h1 class="jumbotron-heading"><?= $page->title() ?></h1>
+			    	<p class="lead text-muted"><?= $page->text()->kirbytext() ?></p>
+			    	<?php if ($site->user()) : ?>
+			    		<p class="text-muted"><a href="<?= $site->url().'/panel/pages/'.$page->uri().'/edit' ?>" target="_blank">Edit</a></p>
+			    	<?php endif ?>
+			    </div>
+			    <div class="col-sm-2 text-center">
+			    	<?php if($page->hasNextVisible()): ?>
+						<a href="<?php echo $page->nextVisible()->url() ?>">
+							<i class="arrow fa fa-caret-right"></i>
+						</a>
+					<?php endif ?>
+			    </div>
+		    </div>
 	    </div>
-    </section>
 
     <div class="album">
 	    <div class="container">
